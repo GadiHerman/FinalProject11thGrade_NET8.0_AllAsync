4c1e90ee6c21a21065aa34f935f901ec949e7a6a
Gadi Herman <gadi.herman@gmail.com>
2023-12-13 12:51:31 +0200

    new repo

---
+++ DBL/CustomerDB.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Models;
+
+namespace DBL
+{
+    public class CustomerDB : BaseDB<Customer>
+    {
+        protected override string GetTableName()
+        {
+            return "Customers";
+        }
+        protected override Customer CreateModel(object[] row)
+        {
+            Customer c = new Customer();
+            c.Id = int.Parse(row[0].ToString());
+            c.Name = row[1].ToString();
+            c.Email = row[2].ToString();
+            c.IsAdmin = bool.Parse(row[4].ToString());
+            return c;
+        }
+        protected override async Task<Customer> CreateModelAsync(object[] row)
+        {
+            Customer c = new Customer();
+            c.Id = int.Parse(row[0].ToString());
+            c.Name = row[1].ToString();
+            c.Email = row[2].ToString();
+            c.IsAdmin = bool.Parse(row[4].ToString());
+            return c;
+        }
+        protected override List<Customer> CreateListModel(List<object[]> rows)
+        {
+            List<Customer> custList = new List<Customer>();
+            foreach (object[] item in rows)
+            {
+                Customer c;
+                c = CreateModel(item);
+                custList.Add(c);
+            }
+            return custList;
+        }
+        protected override async Task<List<Customer>> CreateListModelAsync(List<object[]> rows)
+        {
+            List<Customer> custList = new List<Customer>();
+            foreach (object[] item in rows)
+            {
+                Customer c = new Customer();
+                c = (Customer)CreateModel(item);
+                custList.Add(c);
+            }
+            return custList;
+        }
+
+        protected override async Task<Customer> GetRowByPKAsync(object pk)
+        {
+            string sql = @"SELECT customers.* FROM customers WHERE (CustomerID = @id)";
+            AddParameterToCommand("@id", int.Parse(pk.ToString()));
+            List<Customer> list = (List<Customer>)await SelectAllAsync(sql);
+            if (list.Count == 1)
+                return list[0];
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// TESTED get one customer from Database by primary key 
+        /// </summary>
+        /// <param name="id">SQL string</param>
+        /// <returns>One object of type customer.</returns>
+        protected override Customer GetRowByPK(object pk)
+        {
+            string sql = @"SELECT customers.* FROM customers WHERE (CustomerID = @id)";
+            AddParameterToCommand("@id", int.Parse(pk.ToString()));
+            List<Customer> list = (List<Customer>)SelectAll(sql);
+            if (list.Count == 1)
+                return list[0];
+            else
+                return null;
+        }
+
+        public async Task<List<Customer>> GetAllAsync()
+        {
+            return ((List<Customer>)await SelectAllAsync());
+        }
+
+        public List<Customer> GetAll()
+        {
+            return ((List<Customer>)SelectAll());
+        }
+
+        //public async Task<bool> InsertAsync(Customer customer, string password)
+        //{
+        //    Dictionary<string, string> fillValues = new Dictionary<string, string>
+        //    {
+        //        { "Name", customer.Name },
+        //        { "Email", customer.Email },
+        //        { "CustomerPassword", password }
+        //    };
+        //    return await base.InsertAsync(fillValues) == 1;
+        //}
+
+        //public bool Insert(Customer customer, string password)
+        //{
+        //    Dictionary<string, string> fillValues = new Dictionary<string, string>
+        //    {
+        //        { "Name", customer.Name },
+        //        { "Email", customer.Email },
+        //        { "CustomerPassword", password }
+        //    };
+        //    return base.Insert(fillValues) == 1;
+        //}
+
+        public async Task<Customer> InsertGetObjAsync(Customer customer, string password)
+        {
+            Dictionary<string, string> fillValues = new Dictionary<string, string>()
+            {
+                { "Name", customer.Name },
+                { "Email", customer.Email },
+                { "CustomerPassword", password }
+            };
+            return (Customer)await base.InsertGetObjAsync(fillValues);
+        }
+
+        /// <summary>
+        /// TESTED Insert new cusomer to Data
+        /// </summary>
+        /// <param name="Customer">Customer Object</param>
+        /// <param name="password">password (not part of Customer class)</param>
+        /// <returns>Customer Object with a primary key from the database.</returns>
+        public Customer InsertGetObj(Customer customer, string password)
+        {
+            Dictionary<string, string> fillValues = new Dictionary<string, string>()
+            {
+                { "Name", customer.Name },
+                { "Email", customer.Email },
+                { "CustomerPassword", password }
+            };
+            return (Customer)base.InsertGetObj(fillValues);
+        }
+
+        public async Task<int> UpdateAsync(Customer customer)
+        {
+            Dictionary<string, string> fillValues = new Dictionary<string, string>();
+            Dictionary<string, string> filterValues = new Dictionary<string, string>();
+            fillValues.Add("Name", customer.Name);
+            fillValues.Add("Email", customer.Email);
+            filterValues.Add("CustomerID", customer.Id.ToString());
+            return await base.UpdateAsync(fillValues, filterValues);
+        }
+
+        public int Update(Customer customer)
+        {
+            Dictionary<string, string> fillValues = new Dictionary<string, string>();
+            Dictionary<string, string> filterValues = new Dictionary<string, string>();
+            fillValues.Add("Name", customer.Name);
+            fillValues.Add("Email", customer.Email);
+            filterValues.Add("CustomerID", customer.Id.ToString());
+            return base.Update(fillValues, filterValues);
+        }
+
+        public async Task<int> DeleteAsync(Customer customer)
+        {
+            Dictionary<string, string> filterValues = new Dictionary<string, string>
+            {
+                { "CustomerID", customer.Id.ToString() }
+            };
+            return await base.DeleteAsync(filterValues);
+        }
+        public int Delete(Customer customer)
+        {
+            Dictionary<string, string> filterValues = new Dictionary<string, string>
+            {
+                { "CustomerID", customer.Id.ToString() }
+            };
+            return base.Delete(filterValues);
+        }
+
+        public async Task<int> updatePasswordAsync(Customer customer, string password)
+        {
+            Dictionary<string, string> fillValues = new Dictionary<string, string>();
+            Dictionary<string, string> filterValues = new Dictionary<string, string>();
+            fillValues.Add("Name", customer.Name);
+            fillValues.Add("Email", customer.Email);
+            fillValues.Add("CustomerPassword", password);
+            filterValues.Add("CustomerID", customer.Id.ToString());
+            return await base.UpdateAsync(fillValues, filterValues);
+        }
+
+        public int updatePassword(Customer customer, string password)
+        {
+            Dictionary<string, string> fillValues = new Dictionary<string, string>();
+            Dictionary<string, string> filterValues = new Dictionary<string, string>();
+            fillValues.Add("Name", customer.Name);
+            fillValues.Add("Email", customer.Email);
+            fillValues.Add("CustomerPassword", password);
+            filterValues.Add("CustomerID", customer.Id.ToString());
+            return base.Update(fillValues, filterValues);
+        }
+
+        // specific queries
+        public async Task<string> GetPasswordAsync(int id)
+        {
+            string sql = @"SELECT customers.CustomerPassword FROM customers WHERE (CustomerID = @id)";
+            AddParameterToCommand("@id", id);
+            string oldPassword = (string)await ExecScalarAsync(sql);
+            return oldPassword;
+        }
+
+        public string GetPassword(int id)
+        {
+            string sql = @"SELECT customers.CustomerPassword FROM customers WHERE (CustomerID = @id)";
+            AddParameterToCommand("@id", id);
+            string oldPassword = (string)ExecScalar(sql);
+            return oldPassword;
+        }
+
+        /// <summary>
+        /// TESTED asynchronous version of SelectByPk
+        /// get one customer from Database by primary key 
+        /// </summary>
+        /// <param name="id">SQL string</param>
+        /// <returns>One object of type customer.</returns>
+        public async Task<Customer> SelectByPkAsync(int id)
+        {
+            string sql = @"SELECT customers.* FROM customers WHERE (CustomerID = @id)";
+            AddParameterToCommand("@id", id);
+            List<Customer> list = (List<Customer>)await SelectAllAsync(sql);
+            if (list.Count == 1)
+                return list[0];
+            else
+                return null;
+        }
+
+        public Customer SelectByPk(int id)
+        {
+            string sql = @"SELECT customers.* FROM customers WHERE (CustomerID = @id)";
+            DbParameter p = cmd.CreateParameter();
+            p.ParameterName = "@id";
+            p.Value = id;
+            cmd.Parameters.Add(p);
+            //AddParameterToCommand("@id", id);
+            List<Customer> list = (List<Customer>)SelectAll(sql);
+            if (list.Count == 1)
+                return list[0];
+            else
+                return null;
+        }
+
+        //public async Task<List<Customer>> GetNonAdminsAsync()
+        //{
+        //    Dictionary<string, string> p = new Dictionary<string, string>();
+        //    p.Add("IsAdmin", "0");
+        //    return ((List<Customer>)await SelectAllAsync(p));
+        //}
+
+        //public List<Customer> GetNonAdmins()
+        //{
+        //    Dictionary<string, string> p = new Dictionary<string, string>();
+        //    p.Add("IsAdmin", "0");
+        //    return ((List<Customer>)SelectAll(p));
+        //}
+
+        //public async Task<List<(string, string)>> GetName_Email4NonAdminsAsync()
+        //{
+        //    List<(string, string)> returnList = new List<(string, string)>();
+        //    string sql = "select Name, Email from Customers";
+        //    Dictionary<string, string> p = new Dictionary<string, string>();
+        //    p.Add("IsAdmin", "0");
+        //    List<object[]> list = (List<object[]>)await base.StingListSelectAllAsync(sql, p);
+        //    foreach (object[] item in list)
+        //    {
+        //        string name = item[0].ToString();
+        //        string email = item[1].ToString();
+        //        returnList.Add((name, email));
+        //    }
+        //    return returnList;
+        //}
+
+        //public List<(string, string)> GetName_Email4NonAdmins()
+        //{
+        //    List<(string, string)> returnList = new List<(string, string)>();
+        //    string sql = "select Name, Email from Customers";
+        //    Dictionary<string, string> p = new Dictionary<string, string>();
+        //    p.Add("IsAdmin", "0");
+        //    List<object[]> list = (List<object[]>)base.StingListSelectAll(sql, p);
+        //    foreach (object[] item in list)
+        //    {
+        //        string name = item[0].ToString();
+        //        string email = item[1].ToString();
+        //        returnList.Add((name, email));
+        //    }
+        //    return returnList;
+        //}
+
+        //public async Task<Customer> GetCustomerByOrderIDAsync(int orderID)
+        //{
+        //    string sql = @$"Select mystore.customers.*
+        //                   From mystore.customers Inner Join mystore.orders 
+        //                   On mystore.orders.CustomerID = mystore.customers.CustomerID";
+        //    Dictionary<string, string> p = new Dictionary<string, string>();
+        //    p.Add("mystore.orders.OrderID", orderID.ToString());
+        //    List<Customer> list = (List<Customer>)await SelectAllAsync(sql, p);
+        //    if (list.Count == 1)
+        //        return list[0];
+        //    else
+        //        return null;
+        //}
+
+        //public Customer GetCustomerByOrderID(int orderID)
+        //{
+        //    string sql = @$"Select mystore.customers.*
+        //                   From mystore.customers Inner Join mystore.orders 
+        //                   On mystore.orders.CustomerID = mystore.customers.CustomerID";
+        //    Dictionary<string, string> p = new Dictionary<string, string>();
+        //    p.Add("mystore.orders.OrderID", orderID.ToString());
+        //    List<Customer> list = (List<Customer>)SelectAll(sql, p);
+        //    if (list.Count == 1)
+        //        return list[0];
+        //    else
+        //        return null;
+        //}
+
+    }
+}
---
+++ DBL/OrderDB.cs
@@ -0,0 +1,141 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBL
+{
+    public class OrderDB : BaseDB<Order>
+    {
+        protected override string GetTableName()
+        {
+            return "Orders";
+        }
+        protected override Order CreateModel(object[] row)
+        {
+            Order o = new Order();
+            o.Id = int.Parse(row[0].ToString());
+            o.OrderDateTime = DateTime.Parse(row[1].ToString());
+            int custID = int.Parse(row[2].ToString());
+            o.customer = new CustomerDB().SelectByPk(custID);
+            return o;
+        }
+        protected override async Task<Order> CreateModelAsync(object[] row)
+        {
+            Order o = new Order();
+            o.Id = int.Parse(row[0].ToString());
+            o.OrderDateTime = DateTime.Parse(row[1].ToString());
+            int custID = int.Parse(row[2].ToString());
+            Customer customer = new Customer();
+            CustomerDB customerDB = new CustomerDB();
+            customer = await customerDB.SelectByPkAsync(custID);
+            o.customer = customer;
+            return o;
+        }
+        protected override List<Order> CreateListModel(List<object[]> rows)
+        {
+            List<Order> orderList = new List<Order>();
+            foreach (object[] item in rows)
+            {
+                Order o = new Order();
+                o = (Order)CreateModel(item);
+                orderList.Add(o);
+            }
+            return orderList;
+        }
+        protected override async Task<List<Order>> CreateListModelAsync(List<object[]> rows)
+        {
+            List<Order> orderList = new List<Order>();
+            foreach (object[] item in rows)
+            {
+                Order o = new Order();
+                o = (Order)await CreateModelAsync(item);
+                orderList.Add(o);
+            }
+            return orderList;
+        }
+        protected override async Task<Order> GetRowByPKAsync(object pk)
+        {
+            string sql = @"SELECT orders.* FROM orders WHERE (OrderID = @id)";
+            AddParameterToCommand("@id", int.Parse(pk.ToString()));
+            List<Order> list = (List<Order>)await SelectAllAsync(sql);
+            if (list.Count == 1)
+                return list[0];
+            else
+                return null;
+        }
+
+        protected override Order GetRowByPK(object pk)
+        {
+            string sql = @"SELECT orders.* FROM orders WHERE (OrderID = @id)";
+            AddParameterToCommand("@id", int.Parse(pk.ToString()));
+            List<Order> list = (List<Order>)SelectAll(sql);
+            if (list.Count == 1)
+                return list[0];
+            else
+                return null;
+        }
+
+        public async Task<List<Order>> GetAllAsync()
+        {
+            return (List<Order>)await SelectAllAsync();
+        }
+
+        public async Task<List<Order>> GetAllOrdersByCustomerIDAsync(int customerID)
+        {
+            Dictionary<string, string> p = new Dictionary<string, string>();
+            p.Add("CustomerID", customerID.ToString());
+            return (List<Order>)await SelectAllAsync(p);
+        }
+        public async Task<Order> GetOrderByPkAsync(int OrderID)
+        {
+            Dictionary<string, string> p = new Dictionary<string, string>();
+            p.Add("OrderID", OrderID.ToString());
+            List<Order> list = (List<Order>)await SelectAllAsync(p);
+            if (list.Count == 1)
+                return list[0];
+            else
+                return null;
+        }
+
+        //public async Task<bool> InsertAsync(Order order)
+        //{
+        //    Dictionary<string, string> fillValues = new Dictionary<string, string>();
+        //    DateTime d = DateTime.Now;
+        //    string dts = $"'{d.Year}-{d.Month}-{d.Day} {d.Hour}:{d.Minute}:{d.Second}'";
+        //    fillValues.Add("OrderDateTime", dts);
+        //    fillValues.Add("CustomerID", order.customer.Id.ToString());
+        //    return await base.InsertAsync(fillValues) == 1;
+        //}
+
+        //public Order InsertGetObjAsync(Order order)
+        //{
+        //    Dictionary<string, string> fillValues = new Dictionary<string, string>();
+        //    DateTime d = DateTime.Now;
+        //    string dts = $"'{d.Year}-{d.Month}-{d.Day} {d.Hour}:{d.Minute}:{d.Second}'";
+        //    fillValues.Add("OrderDateTime", dts);
+        //    fillValues.Add("CustomerID", order.customer.Id.ToString());
+        //    return (Order)base.InsertGetObjAsync(fillValues);
+        //}
+
+        public async Task<int> UpdateAsync(Order order)
+        {
+            Dictionary<string, string> fillValues = new Dictionary<string, string>();
+            Dictionary<string, string> filterValues = new Dictionary<string, string>();
+            DateTime d = DateTime.Now;
+            string dts = $"'{d.Year}-{d.Month}-{d.Day} {d.Hour}:{d.Minute}:{d.Second}'";
+            fillValues.Add("OrderDateTime", dts);
+            filterValues.Add("OrderID", order.Id.ToString());
+            return await base.UpdateAsync(fillValues, filterValues);
+        }
+
+        public async Task<int> DeleteAsync(Order order)
+        {
+            Dictionary<string, string> filterValues = new Dictionary<string, string>();
+            filterValues.Add("OrderID", order.Id.ToString());
+            return await base.DeleteAsync(filterValues);
+        }
+    }
+}
